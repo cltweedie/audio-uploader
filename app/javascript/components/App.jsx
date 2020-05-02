b5f18c36f25e0a5ad1b48f583490507ce40d32d2
Chris Tweedie <cltweedie@gmail.com>
2020-05-02 18:28:38 +0100

    Basic UI for recording audio

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { IoIosRecording } from 'react-icons/io';
+import { ReactMic } from '@cleandersonlobo/react-mic';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      record: false,
+      step: 1,
+    };
+
+    this.startRecording = this.startRecording.bind(this);
+    this.stopRecording = this.stopRecording.bind(this);
+  }
+
+  onData(recordedBlob) {
+    console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  onStop(recordedBlob) {
+    console.log('recordedBlob is: ', recordedBlob);
+  }
+
+  startRecording() {
+    this.setState({
+      record: true,
+      step: 2,
+    })
+  }
+
+  stopRecording() {
+    this.setState({
+      step: 3,
+      record: false,
+    })
+  }
+
+  render() {
+    const { record, step } = this.state;
+
+    console.log('record: ', record);
+
+    return (
+      <article className="vh-100 dt w-100 bg-white">
+        <div className="dtc v-mid tc ph3 ph4-l">
+          {step === 1 ? (
+            <h1 className="f6 f2-m f-subheadline-l fw6 tc dark-gray tc">Record something.</h1>
+          ) : (
+            <ReactMic
+              record={record}
+              className="sound-wave"
+              onStop={this.onStop}
+              onData={this.onData}
+              strokeColor={record ? '#000000' : '#FFF'}
+              backgroundColor="#FFF"
+            />
+          )
+          }
+
+          <div>
+            <a
+              onClick={step === 1 ? this.startRecording : this.stopRecording}
+              className="f3 grow no-underline br-pill ph5 pv3 mb2 dib white bg-dark-gray b"
+              style={{ cursor: 'pointer' }}
+            >
+              <IoIosRecording style={{ fontSize: '25px', paddingRight: '5px', marginBottom: '-5px' }}/>
+              {' '}
+              {record ? 'Stop' : 'Start'}
+              {' '}
+              recording
+            </a>
+          </div>
+        </div>
+      </article>
+    );
+  }
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('home'),
+  );
+});
