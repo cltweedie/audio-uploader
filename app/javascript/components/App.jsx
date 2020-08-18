08bbaab98b6b950e7b34ce35dcb8dcb620341cfe
Chris Tweedie <cltweedie@gmail.com>
2020-08-18 09:30:27 +0100

    Add traffic-light color gradient for confidence values

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -124,7 +124,10 @@ class App extends Component {
                   return (
                     <tr key={c[0]}>
                       <td>{c[0].replace(/_/g, ' ')}</td>
-                      <td>{c[1]}%</td>
+                      <td style={{
+                        color: this.confidenceToColor(parseInt(c[1])),
+                        fontWeight: 'bold',
+                      }}>{c[1]}%</td>
                     </tr>
                   )
                 })}
@@ -134,6 +137,39 @@ class App extends Component {
         </FadeIn>
       );
     }
+  }
+
+  confidenceToColor(confidence) {
+    if (confidence > 50) return 'rgb(0, 255, 0)';
+
+    const conf = (confidence * 2) / 100;
+
+    const percentColors = [
+      { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
+      { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+      { pct: 1, color: { r: 0x00, g: 0xff, b: 0 } } ];
+
+    const getColorForPercentage = function(pct) {
+        for (var i = 1; i < percentColors.length - 1; i++) {
+            if (pct < percentColors[i].pct) {
+                break;
+            }
+        }
+        const lower = percentColors[i - 1];
+        const upper = percentColors[i];
+        const range = upper.pct - lower.pct;
+        const rangePct = (pct - lower.pct) / range;
+        const pctLower = 1 - rangePct;
+        const pctUpper = rangePct;
+        const color = {
+            r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+            g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+            b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+        };
+        return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+    }
+
+    return getColorForPercentage(conf);
   }
 
   introHeading = () => (
